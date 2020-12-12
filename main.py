7afa6aeecbd2baaf0a6e67f824c8229f6d32a493
MilanPecov <mpecov@yahoo.ca>
2020-12-12 01:07:03 +0100

    refactor around a Puzzle class

--- main.py
+++ main.py
@@ -1,103 +1,15 @@
-from pprint import pprint
-
-PUZZLE_END = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]  # 4x4
-
-
 class PuzzleSolver:
     def __init__(self, strategy):
+        """
+        :param strategy: Strategy
+        """
         self._strategy = strategy
 
     def run(self):
         self._strategy.do_algorithm()
 
 
-class PuzzleSize:
-    PUZZLE_NUM_ROWS = len(PUZZLE_END)
-    PUZZLE_NUM_COLUMNS = len(PUZZLE_END[0])
-
-
-class StrategyUtils(PuzzleSize):
-    @staticmethod
-    def _to_list(puzzle):
-        """
-        Convert nested tuple to nested list
-        """
-        return [list(row) for row in puzzle]
-
-    @staticmethod
-    def _get_blank_space_coordinates(puzzle):
-        """
-        Returns the i, j coordinates of the 0
-        """
-        i = 0
-        while 0 not in puzzle[i]:
-            i += 1
-        j = puzzle[i].index(0)
-
-        return i, j
-
-    def _swap(self, puzzle, x1, y1, x2, y2):
-        """
-        Swap the positions between two elements
-        """
-        p = self._to_list(puzzle)  # copy the puzzle
-        p[x1][y1], p[x2][y2] = p[x2][y2], p[x1][y1]
-
-        return p
-
-    def _get_moves(self, puzzle):
-        """
-        Returns a list of all the possible moves
-        """
-        moves = []
-        i, j = self._get_blank_space_coordinates(puzzle)
-
-        if i > 0:
-            moves.append(self._swap(puzzle, i, j, i - 1, j))  # move up
-
-        if j < self.PUZZLE_NUM_COLUMNS - 1:
-            moves.append(self._swap(puzzle, i, j, i, j + 1))  # move right
-
-        if j > 0:
-            moves.append(self._swap(puzzle, i, j, i, j - 1))  # move left
-
-        if i < self.PUZZLE_NUM_ROWS - 1:
-            moves.append(self._swap(puzzle, i, j, i + 1, j))  # move down
-
-        return moves
-
-
-class StrategyHeuristics(PuzzleSize):
-    def _heuristic_misplaced(self, puzzle):
-        """
-        Counts the number of misplaced tiles
-        """
-        misplaced = 0
-
-        for i in range(self.PUZZLE_NUM_ROWS):
-            for j in range(self.PUZZLE_NUM_COLUMNS):
-                if puzzle[i][j] != PUZZLE_END[i][j]:
-                    misplaced += 1
-
-        return misplaced
-
-    def _heuristic_manhattan_distance(self, puzzle):
-        """
-        Counts how much is a tile misplaced from the original position
-        """
-        distance = 0
-
-        for i in range(self.PUZZLE_NUM_ROWS):
-            for j in range(self.PUZZLE_NUM_COLUMNS):
-                misplaced_rows_weight = abs(i - (puzzle[i][j] / self.PUZZLE_NUM_ROWS))
-                misplaced_columns_weight = abs(j - (puzzle[i][j] % self.PUZZLE_NUM_COLUMNS))
-
-                distance += misplaced_rows_weight + misplaced_columns_weight
-
-        return distance
-
-
-class Strategy(StrategyUtils, StrategyHeuristics):
+class Strategy:
     num_expanded_nodes = 0
 
     def do_algorithm(self):
@@ -105,8 +17,11 @@ class Strategy(StrategyUtils, StrategyHeuristics):
 
 
 class BreadthFirst(Strategy):
-    def __init__(self, start):
-        self.start = self._to_list(start)
+    def __init__(self, initial_puzzle):
+        """
+        :param initial_puzzle: Puzzle
+        """
+        self.start = initial_puzzle
 
     def do_algorithm(self):
         queue = [[self.start]]
@@ -119,33 +34,40 @@ class BreadthFirst(Strategy):
             queue.pop(0)  # dequeue (FIFO)
             end_node = path[-1]
 
-            if end_node in expanded:
+            if end_node.position in expanded:
                 continue
 
-            for k in self._get_moves(end_node):
-                if k in expanded:
+            for move in end_node.get_moves():
+                if move.position in expanded:
                     continue
-                queue.append(path + [k])  # add new moves at the end of the queue
+                queue.append(path + [move])  # add new path at the end of the queue
 
-            expanded.append(end_node)
+            expanded.append(end_node.position)
             num_expanded_nodes += 1
 
-            if end_node == PUZZLE_END:
+            if end_node.position == end_node.PUZZLE_END_POSITION:
                 break
 
         self.num_expanded_nodes = num_expanded_nodes
         print(f'Breadth First - Expanded nodes: {num_expanded_nodes}')
         print('Solution:')
-        pprint(path)
+        for p in path:
+            print(p)
 
 
 class AStar(Strategy):
-    def __init__(self, start):
-        self.start = self._to_list(start)
-        self.heuristic = self._heuristic_manhattan_distance  # optional: _heuristic_misplaced
+    def __init__(self, initial_puzzle):
+        """
+        :param initial_puzzle: Puzzle
+        """
+        self.start = initial_puzzle
+
+    @staticmethod
+    def _calculate_new_heuristic(move, end_node):
+        return move.heuristic_manhattan_distance() - end_node.heuristic_manhattan_distance()
 
     def do_algorithm(self):
-        queue = [[self.heuristic(self.start), self.start]]
+        queue = [[self.start.heuristic_manhattan_distance(), self.start]]
         expanded = []
         num_expanded_nodes = 0
         path = None
@@ -160,28 +82,140 @@ class AStar(Strategy):
             queue = queue[:i] + queue[i + 1:]
             end_node = path[-1]
 
-            if end_node == PUZZLE_END:
+            if end_node.position == end_node.PUZZLE_END_POSITION:
                 break
-            if end_node in expanded:
+            if end_node.position in expanded:
                 continue
 
-            for k in self._get_moves(end_node):
-                if k in expanded:
+            for move in end_node.get_moves():
+                if move.position in expanded:
                     continue
-                new_path = [path[0] + self.heuristic(k) - self.heuristic(end_node)] + path[1:] + [k]
+                new_path = [path[0] + self._calculate_new_heuristic(move, end_node)] + path[1:] + [move]
                 queue.append(new_path)
-                expanded.append(end_node)
+                expanded.append(end_node.position)
 
             num_expanded_nodes += 1
 
         self.num_expanded_nodes = num_expanded_nodes
         print(f'A* - Expanded nodes: {num_expanded_nodes}')
         print('Solution:')
-        pprint(path)
+        for p in path[1:]:
+            print(p)
+
+
+class Puzzle:
+    def __init__(self, position):
+        """
+        :param position: a list of lists representing the puzzle matrix
+        """
+        self.position = position
+        self.PUZZLE_NUM_ROWS = len(position)
+        self.PUZZLE_NUM_COLUMNS = len(position[0])
+        self.PUZZLE_END_POSITION = self._generate_end_position()
+
+    def __str__(self):
+        """
+        Print in console as a matrix
+        """
+        puzzle_string = '—' * 13 + '\n'
+        for i in range(self.PUZZLE_NUM_ROWS):
+            for j in range(self.PUZZLE_NUM_COLUMNS):
+                puzzle_string += '│{0: >2}'.format(str(self.position[i][j]))
+                if j == self.PUZZLE_NUM_COLUMNS - 1:
+                    puzzle_string += '│\n'
+
+        puzzle_string += '—' * 13 + '\n'
+        return puzzle_string
+
+    def _generate_end_position(self):
+        """
+        Example end position in 4x4 puzzle
+        [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+        """
+        end_position = []
+        new_row = []
+
+        for i in range(self.PUZZLE_NUM_ROWS * self.PUZZLE_NUM_COLUMNS):
+            new_row.append(i)
+            if len(new_row) == self.PUZZLE_NUM_COLUMNS:
+                end_position.append(new_row)
+                new_row = []
+
+        return end_position
+
+    def _swap(self, x1, y1, x2, y2):
+        """
+        Swap the positions between two elements
+        """
+        p = [list(row) for row in self.position]  # copy the puzzle
+        p[x1][y1], p[x2][y2] = p[x2][y2], p[x1][y1]
+
+        return p
+
+    def _get_coordinates(self, tile, position=None):
+        """
+        Returns the i, j coordinates for a given tile
+        """
+        if not position:
+            position = self.position
+
+        for i in range(self.PUZZLE_NUM_ROWS):
+            for j in range(self.PUZZLE_NUM_COLUMNS):
+                if position[i][j] == tile:
+                    return i, j
+
+        return RuntimeError('Invalid tile value')
+
+    def get_moves(self):
+        """
+        Returns a list of all the possible moves
+        """
+        moves = []
+        i, j = self._get_coordinates(0)  # blank space
+
+        if i > 0:
+            moves.append(Puzzle(self._swap(i, j, i - 1, j)))  # move up
+
+        if j < self.PUZZLE_NUM_COLUMNS - 1:
+            moves.append(Puzzle(self._swap(i, j, i, j + 1)))  # move right
+
+        if j > 0:
+            moves.append(Puzzle(self._swap(i, j, i, j - 1)))  # move left
+
+        if i < self.PUZZLE_NUM_ROWS - 1:
+            moves.append(Puzzle(self._swap(i, j, i + 1, j)))  # move down
+
+        return moves
+
+    def heuristic_misplaced(self):
+        """
+        Counts the number of misplaced tiles
+        """
+        misplaced = 0
+
+        for i in range(self.PUZZLE_NUM_ROWS):
+            for j in range(self.PUZZLE_NUM_COLUMNS):
+                if self.position[i][j] != self.PUZZLE_END_POSITION[i][j]:
+                    misplaced += 1
+
+        return misplaced
+
+    def heuristic_manhattan_distance(self):
+        """
+        Counts how much is a tile misplaced from the original position
+        """
+        distance = 0
+
+        for i in range(self.PUZZLE_NUM_ROWS):
+            for j in range(self.PUZZLE_NUM_COLUMNS):
+                i1, j1 = self._get_coordinates(self.position[i][j], self.PUZZLE_END_POSITION)
+                distance += abs(i - i1) + abs(j - j1)
+
+        return distance
 
 
 if __name__ == '__main__':
-    puzzle_start = ((4, 1, 2, 3), (5, 6, 7, 11), (8, 9, 10, 15), (12, 13, 14, 0))
+    puzzle = Puzzle([[4, 1, 2, 3], [5, 6, 7, 11], [8, 9, 10, 15], [12, 13, 14, 0]])
 
-    PuzzleSolver(BreadthFirst(puzzle_start)).run()
-    PuzzleSolver(AStar(puzzle_start)).run()
+    PuzzleSolver(BreadthFirst(puzzle)).run()
+    PuzzleSolver(AStar(puzzle)).run()
--- tests.py
+++ tests.py
@@ -1,49 +1,76 @@
-from main import Strategy, PuzzleSolver, AStar, BreadthFirst
+from main import PuzzleSolver, AStar, BreadthFirst, Puzzle
+
+
+def test_generate_end_position():
+    puzzle_4x4 = Puzzle([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]])
+
+    assert puzzle_4x4.PUZZLE_END_POSITION == [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]], '4x4'
+
+    puzzle_3x3 = Puzzle([[0, 2, 1], [3, 5, 4], [6, 7, 8], [9, 10, 11]])
+    assert puzzle_3x3.PUZZLE_END_POSITION == [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]], '3x3'
 
 
 def test_swap():
-    puzzle = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-    s = Strategy()
-    new_position = s._swap(puzzle, 0, 0, 0, 1)
+    puzzle = Puzzle([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]])
+    new_position = puzzle._swap(0, 0, 0, 1)
 
     assert new_position == [[2, 1, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
 
 
-def test_get_blank_space_coordinates():
-    puzzle = [[1, 2, 6, 3], [4, 9, 5, 7], [8, 13, 11, 15], [12, 14, 0, 10]]
-    s = Strategy()
-    i, j = s._get_blank_space_coordinates(puzzle)
+def test_get_coordinates():
+    puzzle = Puzzle([[1, 2, 6, 3], [4, 9, 5, 7], [8, 13, 11, 15], [12, 14, 0, 10]])
+    i, j = puzzle._get_coordinates(0)
 
     assert i == 3
     assert j == 2
 
+    i, j = puzzle._get_coordinates(10)
+    assert i == 3
+    assert j == 3
+
 
 def test_all_possible_moves():
-    puzzle = ((1, 2, 3, 4), (5, 6, 0, 7), (8, 9, 10, 11), (12, 13, 14, 15))
-    s = Strategy()
-    output = s._get_moves(puzzle)
+    puzzle = Puzzle([[1, 2, 3, 4], [5, 6, 0, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
+    output = puzzle.get_moves()
 
-    assert output[0] == [[1, 2, 0, 4], [5, 6, 3, 7], [8, 9, 10, 11], [12, 13, 14, 15]], 'up'
-    assert output[1] == [[1, 2, 3, 4], [5, 6, 7, 0], [8, 9, 10, 11], [12, 13, 14, 15]], 'right'
-    assert output[2] == [[1, 2, 3, 4], [5, 0, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]], 'left'
-    assert output[3] == [[1, 2, 3, 4], [5, 6, 10, 7], [8, 9, 0, 11], [12, 13, 14, 15]], 'down'
+    assert output[0].position == [[1, 2, 0, 4], [5, 6, 3, 7], [8, 9, 10, 11], [12, 13, 14, 15]], 'up'
+    assert output[1].position == [[1, 2, 3, 4], [5, 6, 7, 0], [8, 9, 10, 11], [12, 13, 14, 15]], 'right'
+    assert output[2].position == [[1, 2, 3, 4], [5, 0, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]], 'left'
+    assert output[3].position == [[1, 2, 3, 4], [5, 6, 10, 7], [8, 9, 0, 11], [12, 13, 14, 15]], 'down'
+
+
+def test_heuristic_misplaced():
+    puzzle = Puzzle([[1, 2, 0, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
+    misplaced = puzzle.heuristic_misplaced()
+
+    assert misplaced == 3
+
+
+def test_heuristic_manhattan_distance():
+    puzzle = Puzzle([[1, 2, 6, 3], [4, 9, 5, 7], [8, 13, 11, 15], [12, 14, 0, 10]])
+    distance = puzzle.heuristic_manhattan_distance()
+
+    assert distance == 16
 
 
 def test_performance():
-    puzzle_start = ((4, 1, 2, 3), (5, 6, 7, 0), (8, 9, 10, 11), (12, 13, 14, 15))
-
-    s1 = PuzzleSolver(AStar(puzzle_start))
-    s1.run()
-    assert s1._strategy.num_expanded_nodes == 4
+    puzzle_start = Puzzle([[4, 1, 2, 3], [5, 6, 7, 0], [8, 9, 10, 11], [12, 13, 14, 15]])
 
     s2 = PuzzleSolver(BreadthFirst(puzzle_start))
     s2.run()
     assert s2._strategy.num_expanded_nodes == 35
 
+    s1 = PuzzleSolver(AStar(puzzle_start))
+    s1.run()
+    assert s1._strategy.num_expanded_nodes == 4
+
 
 if __name__ == "__main__":
+    test_generate_end_position()
     test_swap()
-    test_get_blank_space_coordinates()
+    test_get_coordinates()
     test_all_possible_moves()
+    test_heuristic_misplaced()
+    test_heuristic_manhattan_distance()
     test_performance()
     print("Everything passed")
