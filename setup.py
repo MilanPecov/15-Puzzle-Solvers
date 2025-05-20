8826e6314378d4da29e3ce3e9055cfe4b9651eb2
Milan Pecov <milan@showpass.com>
2025-05-19 21:17:15 -0600

    bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fifteen_puzzle_solvers',
-    version='2.0.2',
+    version='2.0.3',
     author='Milan Pecov',
     author_email='mpecov@yahoo.ca',
     description='Different solvers for the 15 puzzle sliding game',
